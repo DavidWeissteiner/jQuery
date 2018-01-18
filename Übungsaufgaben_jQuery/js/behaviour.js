13e9e180a5e8e6a3604a8917f64cc1eaf8f6d364
David Weissteiner <david.weissteiner@gmail.com>
2018-01-18 12:33:56 +0100

    finished all

--- Übungsaufgaben_jQuery/js/behaviour.js
+++ Übungsaufgaben_jQuery/js/behaviour.js
@@ -1,3 +1,47 @@
 
 
 
+
+$("#block3").hide();
+
+$("#steuern_rot").click(function()
+{
+    $("#block1").hide();
+});
+
+
+$("#steuern_gruen").click(function()
+{
+    $("#block2").hide("slow");
+});
+
+
+$("#steuern_blau").click(function()
+{
+    $("#block3").show();
+});
+
+
+
+
+var inputNumber;
+var randomNumber;
+
+$("#absenden").click(function()
+{
+    inputNumber=parseInt($("#eingegebeneZahl").val());
+    randomNumber = _.random(0, 100);
+    if(inputNumber<randomNumber)
+    {
+        $("#ausgabe").text("Ihre Zahl war leider zu klein!")
+    }
+    else if(inputNumber>randomNumber)
+    {
+        $("#ausgabe").text("Ihre Zahl war leider zu groß!")
+    }
+    else if(inputNumber==randomNumber)
+    {
+        $("#ausgabe").text("Geschafft! Sie haben die Zahl erraten.")
+    }
+    alert(randomNumber);
+});
